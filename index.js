cab1764cef0cf0ca700cf7f84c54a5ae4c275914
Mike Frey <mike.frey@tstmedia.com>
2012-10-04 10:34:40 -0500

    removed another middleware reference

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ exports.App = require('./app')
 exports.Controller = require('./controller')
 exports.Templating = require('./templating')
 exports.Router = require('./router')
-exports.Middleware = require('./middleware')
 exports.Plugin = require('./plugin')
 
 exports.plugins = require('./plugins')
