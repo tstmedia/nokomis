8fc925d87bd47a8ba11d91e9e96437a28726ad9f
Darcy Murphy <mrDarcyMurphy@gmail.com>
2012-10-19 12:28:41 -0500

    wrote the tests for the proposed routing syntax

--- test/router_test.js
+++ test/router_test.js
@@ -1,3 +1,13 @@
+// Restful Routes, according to Rails: http://guides.rubyonrails.org/routing.html
+// HTTP   Verb Path         action  used for
+// ****** ***************** ******* ********
+// GET    /photos           index   display a list of all photos
+// GET    /photos/new       new     return an HTML form for creating a new photo
+// POST   /photos           create  create a new photo
+// GET    /photos/:id       show    display a specific photo
+// GET    /photos/:id/edit  edit    return an HTML form for editing a photo
+// PUT    /photos/:id       update  update a specific photo
+// DELETE /photos/:id       destroy delete a specific photo
 
 var assert = require('assert')
 var sinon = require('sinon')
@@ -38,6 +48,60 @@ describe('Router', function() {
       assert.equal(router.routes['/my/:route'], handler)
       done()
     })
+
+    it('should correctly register a route with object syntax and action string', function(done) {
+      var handler = {controller:'controller', action:'index'}
+      router.register({ '/my/:route': handler })
+      assert.equal(router.routes['/my/:route'], handler)
+      done()
+    })
+
+    it('should correctly register a route with object syntax and method only', function(done) {
+      var handler = {controller:'controller', method:'GET'}
+      router.register({ '/my/:route': handler })
+      assert.equal(router.routes['/my/:route'], handler)
+      done()
+    })
+
+    it('should correctly register a route with object syntax, action key, and single method', function(done) {
+      var handler = {controller:'controller', index:'GET'}
+      router.register({ '/my/:route': handler })
+      assert.equal(router.routes['/my/:route'], handler)
+      done()
+    })
+
+    it('should correctly register a route with object syntax, action key, and method array', function(done) {
+      var handler = {controller:'controller', index:['GET','POST']}
+      router.register({ '/my/:route': handler })
+      assert.equal(router.routes['/my/:route'], handler)
+      done()
+    })
+
+    it('should correctly register a route with object syntax, many action keys, and method strings', function(done) {
+      var handler = {controller:'controller', index:'GET', create:'POST'}
+      router.register({ '/my/:route': handler })
+      assert.equal(router.routes['/my/:route'], handler)
+      done()
+    })
+
+    it('should fail to register a route with object syntax, action keys, and conflicting methods', function(done) {
+      var handler = {controller:'controller', index:['GET','POST'], create:'POST'}
+      router.register({ '/my/:route': handler })
+      assert.equal(router.routes['/my/:route'], handler)
+      assert.equal('passes', false) // <- forcing a failure until this throws a proper error
+      done()
+    })
+
+    // Actually, given the above syntax, then this should overwrite the declaration, then I don't have to waste my time munging things together
+    it('should overwrite a route declared multiple times', function(done) {
+      var handler1 = {controller:'controller', index:'GET'}
+      var handler2 = {controller:'controller', create:'POST'}
+      router.register('/my/:id', handler1)
+      router.register('/my/:id', handler2)
+      assert.equal(router.routes['/my/:id'], handler2)
+      done()
+    })
+
   })
 
   describe('Match', function(){
@@ -108,33 +172,59 @@ describe('Router', function() {
       done()
     })
 
-    it('should match different methods to different actions on the same controller', function(){
-      // register all the routes first, as you would in an app
-      var handler1 = {controller:'controller', action:'index',  method:'GET'}
-      var handler2 = {controller:'controller', action:'create', method:'POST'}
-      router.register('/your/:id', handler1)
-      // router.routes Object
-      //   /your/:id: Object
-      //     action: "index"
-      //     controller: "controller"
-      //     method: "GET"
-      router.register('/your/:id', handler2)
-      // router.routes Object
-      //   /your/:id: Object
-      //     action: "create"
-      //     controller: "controller"
-      //     method: "POST"
 
+    it('should match single HTTP method on action key', function(done) {
+      var handler = {controller:'controller', index:'GET'}
+      router.register('/your/:id', handler)
 
       var match = router.match({url:'/your/value', method:'GET'})
-      assert(match)
       assert.equal(typeof match, 'object')
       assert.equal(match.controller, controller)
+      assert.equal(match.action, 'index')
+      done()
+    })
+
+    it('should match HTTP method array on action key', function(done) {
+      var handler = {controller:'controller', index:'GET'}
+      router.register('/your/:id', handler)
+
+      var match = router.match({url:'/your/value', method:'GET'})
+      assert.equal(typeof match, 'object')
+      assert.equal(match.controller, controller)
+      assert.equal(match.action, 'index')
+      done()
+    })
+
+    it('should match HTTP method array on action key', function(done) {
+      var handler = {controller:'controller', index:'GET', create:'POST'}
+      router.register('/your/:id', handler)
+
+      var match = router.match({url:'/your/value', method:'GET'})
+      assert.equal(typeof match, 'object')
+      assert.equal(match.controller, controller)
+      assert.equal(match.action, 'index')
+
+      match = router.match({url:'/your/value', method:'POST'})
+      assert.equal(typeof match, 'object')
+      assert.equal(match.controller, controller)
+      assert.equal(match.action, 'create')
+      done()
+    })
+
+    it('should match the controller method defined last', function(done){
+      var handler1 = {controller:'controller', index:'GET'}
+      var handler2 = {controller:'controller', create:'POST'}
+      router.register('/your/:id', handler1)
+      router.register('/your/:id', handler2)
+
+      var match = router.match({url:'/your/value', method:'GET'})
+      assert(!match)
 
       match = router.match({url:'/your/value', method:'POST'})
       assert(match)
       assert.equal(typeof match, 'object')
       assert.equal(match.controller, controller)
+      assert.equal(match.action, 'create')
       done()
     })
 
